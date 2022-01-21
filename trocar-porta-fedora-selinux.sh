ab183b08f3cdb44171abb818ecdc9ec2d4ae4fe6
d0wr1k <69050859+d0wr1k@users.noreply.github.com>
2022-01-21 13:46:09 -0300

    Update trocar-porta-fedora-selinux.sh

--- trocar-porta-fedora-selinux.sh
+++ trocar-porta-fedora-selinux.sh
@@ -4,5 +4,6 @@ sudo firewall-cmd --add-port=6550/tcp --permanent
 sudo firewall-cmd --reload
 sudo firewall-cmd --remove-service=ssh --permanent
 sudo firewall-cmd --reload
+sed -i 's/22/6500/g' /etc/ssh/sshd_config
 sudo systemctl restart sshd
 netstat -tunl | grep 6550
